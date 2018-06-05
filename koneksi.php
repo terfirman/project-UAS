42b1e10858edcad1359b2a02713bba7fcaa8443a
terfirman <terfirman@gmail.com>
2018-06-05 14:45:19 +0700

    upload

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,9 @@
 <?php
     class Koneksi{
         private $server = "localhost";
-        private $username = "root";
+        private $username = "id6072170_mahasiswa";
         private $password = "";
-        private $db = "mahasiswa1";
+        private $db = "id6072170_uasweb";
 
         function getKoneksi(){
             return new mysqli($this->server, $this->username, $this->password, $this->db);
